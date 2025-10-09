572952ceae0f028f00e1cc728341e6124e6f7c09
AN-Fahmy <fahmyan@taibaconnect.com>
2025-10-09 17:29:21 +0300

    design responsive

--- src/app/components/ai-cta-section/ai-cta-section.ts
+++ src/app/components/ai-cta-section/ai-cta-section.ts
@@ -7,20 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './ai-cta-section.scss'
 })
 export class AiCtaSection {
-  ngOnInit() {
-    const particles = document.querySelectorAll('.particle');
-    particles.forEach((p: any) => {
-      const xStart = Math.random() * window.innerWidth;
-      const yStart = Math.random() * window.innerHeight;
-      const xEnd = xStart + (Math.random() * 100 - 50);
-      const yEnd = yStart + (Math.random() * 80 - 40);
-      const delay = Math.random() * 5;
 
-      p.style.setProperty('--x-start', `${xStart}px`);
-      p.style.setProperty('--y-start', `${yStart}px`);
-      p.style.setProperty('--x-end', `${xEnd}px`);
-      p.style.setProperty('--y-end', `${yEnd}px`);
-      p.style.animationDelay = `${delay}s`;
-    });
-  }
 }
